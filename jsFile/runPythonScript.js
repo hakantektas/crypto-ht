f09c8367727e84c33532f343cb8179a5e6795b8a
hakantektas <72494835+hakantektas@users.noreply.github.com>
2024-05-02 14:10:01 +0300

    update new

--- jsFile/runPythonScript.js
+++ jsFile/runPythonScript.js
@@ -1,31 +1,47 @@
 const { exec } = require('child_process');
 
 function runPythonScript() {
-    console.log("Python script çalıştırılıyor...");
-
-    // Python betiğini çalıştırırken kullanıcıya bir "yükleme" göstergesi sağlayın
-    const interval = setInterval(() => {
-        process.stdout.write(".");
-    }, 1000); // Her saniyede bir nokta yazdır
-
     return new Promise((resolve, reject) => {
-        exec('python3 ./pythonFile/crypto_rsi.py', (error, stdout, stderr) => {
-            clearInterval(interval); // Nokta yazdırmayı durdur
-            console.log(""); // Yeni bir satır
-            if (error) {
-                reject(error);
-                return;
+        // Hareketli loading animasyonunu oluştur
+        const frames = ['|', '/', '-', '\\'];
+        let frameIndex = 0;
+        const loadingInterval = setInterval(() => {
+            process.stdout.clearLine(); // Önceki satırı temizle
+            process.stdout.cursorTo(0); // İmleci satırın başına taşı
+            process.stdout.write('Loading ' + frames[frameIndex]); // Yükleme animasyonunu yazdır
+            frameIndex = (frameIndex + 1) % frames.length; // Bir sonraki çerçeve için indeksi güncelle
+        }, 100); // Her 100 milisaniyede bir çerçeve değiştir
+
+        const pythonProcess = exec('python3 ./pythonFile/crypto_rsi.py');
+
+        pythonProcess.stdout.on('data', (data) => {
+            // Python betiğinden gelen çıktıyı konsola yazdır
+            console.log(data.toString().trim());
+        });
+
+        pythonProcess.stderr.on('data', (data) => {
+            // Hata mesajlarını konsola yazdır
+            console.error(data.toString().trim());
+        });
+
+        pythonProcess.on('close', (code) => {
+            clearInterval(loadingInterval); // Yükleme animasyonunu durdur
+            process.stdout.clearLine(); // Satırı temizle
+            process.stdout.cursorTo(0); // İmleci sıfıra taşı
+
+            if (code === 0) {
+                resolve("Python betiği başarıyla tamamlandı.");
+            } else {
+                reject(`Python betiği bir hata ile sonlandı: ${code}`);
             }
-            resolve(stdout);
         });
     });
 }
 
 runPythonScript()
-    .then((output) => {
-        console.log(output);
+    .then((message) => {
+        console.log(message);
     })
     .catch((error) => {
-        console.error('Python script error:', error);
+        console.error('Python betiği çalıştırılırken bir hata oluştu:', error);
     });
-
