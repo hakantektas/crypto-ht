55b4fe532b8b05af8a145b79743323d7fa5ac866
hakantektas <72494835+hakantektas@users.noreply.github.com>
2024-05-02 12:32:59 +0300

    update new

--- jsFile/runPythonScript.js
+++ jsFile/runPythonScript.js
@@ -2,7 +2,7 @@ const { exec } = require('child_process');
 
 function runPythonScript() {
     return new Promise((resolve, reject) => {
-        exec('python3 /Users/hakantektas/Desktop/cryto-ht/pythonFile/crypto_rsi.py', (error, stdout, stderr) => {
+        exec('python3 ./pythonFile/crypto_rsi.py', (error, stdout, stderr) => {
             if (error) {
                 reject(error);
                 return;
