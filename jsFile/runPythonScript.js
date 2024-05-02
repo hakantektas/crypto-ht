b709d94102a1af065467517c8a2b4cce3e773201
hakantektas <72494835+hakantektas@users.noreply.github.com>
2024-05-02 13:14:27 +0300

    update new

--- jsFile/runPythonScript.js
+++ jsFile/runPythonScript.js
@@ -1,8 +1,17 @@
 const { exec } = require('child_process');
 
 function runPythonScript() {
+    console.log("Python script çalıştırılıyor...");
+
+    // Python betiğini çalıştırırken kullanıcıya bir "yükleme" göstergesi sağlayın
+    const interval = setInterval(() => {
+        process.stdout.write(".");
+    }, 1000); // Her saniyede bir nokta yazdır
+
     return new Promise((resolve, reject) => {
         exec('python3 ./pythonFile/crypto_rsi.py', (error, stdout, stderr) => {
+            clearInterval(interval); // Nokta yazdırmayı durdur
+            console.log(""); // Yeni bir satır
             if (error) {
                 reject(error);
                 return;
@@ -19,3 +28,4 @@ runPythonScript()
     .catch((error) => {
         console.error('Python script error:', error);
     });
+
